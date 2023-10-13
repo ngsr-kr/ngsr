13cfe228c055d0534df4c6d2e34e65c3cc74827f
cozynye <cozynye@gmail.com>
2023-10-14 01:13:47 +0900

    add: 헤더 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import Header from "./_component/layout/Header";
 
 export const metadata: Metadata = {
